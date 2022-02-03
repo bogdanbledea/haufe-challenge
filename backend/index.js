bc9d4202317fd2d258200dfc170572e03c080acb
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2022-02-03 14:14:31 +0200

    Fix nodejs connection to postgres inside docker

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432
